25fdd084f907b74d4a2a8a1854e00431cd1e33bd
Hikirangi <dzhunall@mail.ru>
2019-11-14 23:17:43 +0300

    [ADD]Start

---
+++ Crunchers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Crunchers.Startup))]
+namespace Crunchers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
