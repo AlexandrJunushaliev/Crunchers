0b34ee19aa0abbc99112a9ca672897af456f45d1
Hikirangi <dzhunall@mail.ru>
2019-11-15 01:06:30 +0300

    [ADD] Roles Management, admin controller and example of  page

---
+++ Crunchers/Controllers/AdminController.cs
@@ -0,0 +1,41 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace Crunchers.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class AdminController : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public AdminController()
+        {
+        }
+
+        public AdminController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public ApplicationSignInManager SignInManager
+        {
+            get => _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            private set => _signInManager = value;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get => _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            private set => _userManager = value;
+        }
+        // GET
+        public ActionResult ManageShop()
+        {
+            return View();
+        }
+    }
+}
