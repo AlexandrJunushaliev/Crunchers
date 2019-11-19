f6981711f0638571963b56bd5c42347a7dc317bb
Hikirangi <dzhunall@mail.ru>
2019-11-19 04:30:45 +0300

    [ADD]SearchRedirect

--- Crunchers/Controllers/HomeController.cs
+++ Crunchers/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace Crunchers.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
---
+++ Crunchers/Controllers/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Crunchers.Controllers
+{
+    public class A
+    {
+        public string text;
+    }
+    public class SearchController : Controller
+    {
+        // GET
+        public ActionResult Index(string text)
+        {
+            var a =new A(){text = text};
+            return View(a);
+        }
+    }
+}
