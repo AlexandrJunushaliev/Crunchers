304dfc8bb690ab0df7c54fa170e69deb05e47302
Hikirangi <dzhunall@mail.ru>
2019-12-02 20:13:38 +0300

    [ADD]Catalog

--- Crunchers/Controllers/CatalogController.cs
+++ Crunchers/Controllers/CatalogController.cs
@@ -24,6 +24,7 @@ namespace Crunchers.Controllers
             public string CharacteristicName;
             public int CharacteristicId;
             public int FilterId;
+            public bool isRanged;
 
             public override bool Equals(object obj)
             {
@@ -39,7 +40,7 @@ namespace Crunchers.Controllers
         public class CatalogProductsResponse
         {
             public IEnumerable<IGrouping<string, CatalogFiltersResponse>> Filters;
-            public IEnumerable<ProductModel> Product;
+            public IEnumerable<ProductModel> Products;
         }
 
         public async Task<ActionResult> CategoryProducts(int categoryId, [FromUri] string[] filterValues = null,
@@ -58,24 +59,31 @@ namespace Crunchers.Controllers
             var products = filterValues != null
                 ? await new ProductModel().FilterProducts(filterValuesTuples, filterModels, categoryId)
                 : await new ProductModel().GetProductsByCategoryId(categoryId);
-            
+
             var filters = products.Join(filterModels, x => x.CharacteristicId, y => y.CharacteristicId, (x, y) => new
-            {
-                y.From,
-                y.To,
-                y.CharacteristicId,
-                y.CharacteristicName,
-                x.ValueToCharName,
-                y.FilterId
-            }).Where(x=>x.From<0||x.From>=0&&x.ValueToCharName.Item2<=x.To&&x.ValueToCharName.Item2>=x.From).Select(x => new CatalogFiltersResponse()
-            {
-                IsEnabled = filterValues != null && filterValuesTuples.Select(y => y.Item1).Contains(x.FilterId),
-                FilterShowValue = x.From >= 0 ? $"от {x.From} до {x.To}" : $"{x.ValueToCharName.Item2}",
-                CharacteristicName = x.CharacteristicName,
-                CharacteristicId = x.CharacteristicId,
-                FilterId = x.FilterId
-            })/*.Distinct()*/.GroupBy(x => x.CharacteristicName);
-            return View();
+                {
+                    y.From,
+                    y.To,
+                    y.CharacteristicId,
+                    y.CharacteristicName,
+                    x.ValueToCharName,
+                    y.FilterId
+                })
+                .Where(x => x.From < 0 ||
+                            x.From >= 0 && x.ValueToCharName.Item2 <= x.To && x.ValueToCharName.Item2 >= x.From).Select(
+                    x => new CatalogFiltersResponse()
+                    {
+                        IsEnabled =
+                            filterValues != null && filterValuesTuples.Select(y => y.Item1).Contains(x.FilterId),
+                        FilterShowValue = x.From >= 0 ? $"от {x.From} до {x.To}" : $"{x.ValueToCharName.Item2}",
+                        CharacteristicName = x.CharacteristicName,
+                        CharacteristicId = x.CharacteristicId,
+                        FilterId = x.FilterId,
+                        isRanged = !(x.From<0)
+                    })
+                .Distinct().GroupBy(x => x.CharacteristicName);
+            var response = new CatalogProductsResponse(){Products =products,Filters = filters};
+            return View(response);
         }
     }
 }
